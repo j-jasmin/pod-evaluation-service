1e5d526c5c3ac3b4603c61d5edc2e6fc2cf842de
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-10 00:30:15 +0100

    Update variable naming

--- rules/age.js
+++ rules/age.js
@@ -1,7 +1,7 @@
 module.exports = (pod) => {
   const currentTime = new Date();
-  const xDaysAgo = new Date(Date.now()).setDate(currentTime.getDate() - 7);
+  const SevenDaysAgo = new Date(Date.now()).setDate(currentTime.getDate() - 7);
   const startTimeDate = new Date(pod.status.startTime);
 
-  return xDaysAgo < startTimeDate ? true : false;
+  return SevenDaysAgo < startTimeDate ? true : false;
 };
